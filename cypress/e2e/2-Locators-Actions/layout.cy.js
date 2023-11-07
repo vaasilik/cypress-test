31fdb177858e11db7e556b254de010e57c739ac2
vaasilik <vs.marina49@gmail.com>
2023-11-07 09:23:18 +0200

    homework number 12 with last fix

--- cypress/e2e/2-Locators-Actions/layout.cy.js
+++ cypress/e2e/2-Locators-Actions/layout.cy.js
@@ -1,19 +1,21 @@
 describe("Verify stepper layout", () => {
-    it("Page /pages/layout/stepper", () => {
-      cy.visit("/pages/layout/stepper");
-  
-      // Функція для перевірки тексту на стеці та натискання кнопки "NEXT"
+  it("Page /pages/layout/stepper", () => {
+    cy.visit("/pages/layout/stepper");
+
+    // Функція для перевірки тексту на стеці та натискання кнопки "Next"
     function checkStepText(stepText) {
       cy.get(".step-content").should("contain", stepText);
-      cy.get("body > ngx-app > ngx-pages > ngx-one-column-layout > nb-layout > div > div > div > div > div > nb-layout-column > ngx-components > ngx-stepper > nb-card > nb-card-body > nb-stepper > div.step-content > button:nth-child(3)").click();
+      if (stepText !== "Step content #4") {
+        cy.get('.appearance-filled:contains("next"):eq(0)').click();
+      }
     }
 
-    // Перевіряємо текст на кожному стеці та натискаємо кнопку "NEXT"
+    // Перевіряємо текст на кожному стеці та натискаємо кнопку "Next"
     checkStepText("Step content #1");
     checkStepText("Step content #2");
     checkStepText("Step content #3");
 
-    // На останньому стеці кнопка "NEXT" не активна, тому її не перевіряємо та не натискаємо
+    // На останньому стеці кнопка "Next" не активна, тому її не перевіряємо та не натискаємо
     cy.get(".step-content").should("contain", "Step content #4");
   });
 });
