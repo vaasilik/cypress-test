21368bf4367cba9b6e521e4589daf1a7c4d6f6bf
vaasilik <vs.marina49@gmail.com>
2023-11-07 21:44:03 +0200

    register page tests

---
+++ cypress/e2e/3-Register-page/1-filling.cy.js
@@ -0,0 +1,23 @@
+describe('Registration Form Test 1', () => {
+    it('should fill out the registration form and check the checkbox', () => {
+ 
+      cy.visit('/auth/register');
+
+      cy.get('[name="fullName"]').type('Maryna Vasylyk');
+      cy.get('[name="email"]').type('marynav@example.com');
+      cy.get('[name="password"]').type('password123');
+      cy.get('[name="rePass"]').type('password123');
+
+      cy.get('span.custom-checkbox').click();
+  
+      cy.get('[name="fullName"]').should('have.value', 'Maryna Vasylyk');
+      cy.get('[name="email"]').should('have.value', 'marynav@example.com');
+      cy.get('[name="password"]').should('have.value', 'password123');
+      cy.get('[name="rePass"]').should('have.value', 'password123');
+  
+      cy.get('.btn-disabled').should('not.exist');
+  
+      cy.contains('Register').click();
+    });
+  });
+  
