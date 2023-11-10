6a31155fa7c13ec04176fe5f290acf4e2c15f1a6
vaasilik <vs.marina49@gmail.com>
2023-11-10 23:02:10 +0200

    tests for tables page

--- cypress/e2e/3-Register-page/1-filling.cy.js
+++ cypress/e2e/3-Register-page/1-filling.cy.js
@@ -1,23 +1,22 @@
 describe('Registration Form Test 1', () => {
-    it('should fill out the registration form and check the checkbox', () => {
- 
+  it('should fill out the registration form and check the checkbox', () => {
       cy.visit('/auth/register');
 
-      cy.get('[name="fullName"]').type('Maryna Vasylyk');
-      cy.get('[name="email"]').type('marynav@example.com');
-      cy.get('[name="password"]').type('password123');
-      cy.get('[name="rePass"]').type('password123');
+      cy.get('body').within(() => {
+          cy.get('[name="fullName"]').type('Maryna Vasylyk');
+          cy.get('[name="email"]').type('marynav@example.com');
+          cy.get('[name="password"]').type('password123');
+          cy.get('[name="rePass"]').type('password123');
 
-      cy.get('span.custom-checkbox').click();
-  
-      cy.get('[name="fullName"]').should('have.value', 'Maryna Vasylyk');
-      cy.get('[name="email"]').should('have.value', 'marynav@example.com');
-      cy.get('[name="password"]').should('have.value', 'password123');
-      cy.get('[name="rePass"]').should('have.value', 'password123');
-  
-      cy.get('.btn-disabled').should('not.exist');
-  
-      cy.contains('Register').click();
-    });
+          cy.get('span.custom-checkbox').click();
+
+          cy.get('[name="fullName"]').should('have.value', 'Maryna Vasylyk');
+          cy.get('[name="email"]').should('have.value', 'marynav@example.com');
+          cy.get('[name="password"]').should('have.value', 'password123');
+          cy.get('[name="rePass"]').should('have.value', 'password123');
+          cy.get('.btn-disabled').should('not.exist');
+
+          cy.contains('Register').click();
+      });
   });
-  
+});
